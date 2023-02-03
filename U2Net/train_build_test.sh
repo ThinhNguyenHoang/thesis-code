623d7d71aac01449460d94dacbc27a7ed2a29d75
thinh.nh <thinh.nh@holistics.io>
2023-02-03 11:43:03 +0700

    update build code and requirements for opencv only

--- U2Net/train_build_test.sh
+++ U2Net/train_build_test.sh
@@ -1,7 +1,7 @@
 export PROJECT_ID=thesis-372616
 export REPO_NAME=thesis-registry
-export IMAGE_NAME=test
-export IMAGE_TAG=test
+export IMAGE_NAME=base-cuda
+export IMAGE_TAG=tensorflow-opencv
 export IMAGE_URI=us-central1-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG}
 
 docker build -f Dockerfile -t ${IMAGE_URI} ./
